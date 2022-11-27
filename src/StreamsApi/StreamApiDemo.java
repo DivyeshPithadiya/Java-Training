15ef9b166c32bdce29a29e0161b851c88aba5aa2
DivyeshPithadiya <divyeshpithadiya3@gmail.com>
2022-11-27 12:01:25 +0530

    Stream API implementataion

--- src/StreamsApi/StreamApiDemo.java
+++ src/StreamsApi/StreamApiDemo.java
@@ -40,6 +40,41 @@ public class StreamApiDemo {
 		// min or max
 		Integer a=list.stream().min((o1,o2)->o1-o2).get();	
 		System.out.println(a);
+				 
+		List<Integer> rollNo  = new ArrayList<>();
+		rollNo.add(1);
+		rollNo.add(2);
+		rollNo.add(3);
+		rollNo.add(4);
+		rollNo.add(5);
+		
+	
+		List evenNo = 
+						rollNo
+						.stream()
+						.filter((roll)->roll%2==0)
+						.collect(Collectors.toList());
+				
+		List demo = rollNo
+						.stream()
+						.filter(
+								(roll)->roll==3)
+						.map(roll->roll+7)
+						.collect(Collectors.toList());
+				
+
+		List res = rollNo.stream().map( roll -> 
+						roll==3 ? roll+7 : roll
+						).collect(Collectors.toList());
+				
+		int sum = rollNo.stream().reduce(1,
+				(e1,e2)->e1*e2);
+				
+		System.out.println(sum);
+				
+		System.out.println(demo);
+				
+				
 	}
 
 }
